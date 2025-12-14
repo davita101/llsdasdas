c4e3082c0d8a68b08f240623cc7c9fdfea1a17e3
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-12-14 08:35:13 +0400

    asdas

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,4 +1,3 @@
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
@@ -23,7 +22,30 @@ export function RegisterForm({
     password: "",
     confirmPassword: "",
   });
-
+  // ! AUTH
+  const handleRegister = async () => {
+    if (userInfo.password !== userInfo.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    console.log("lomi");
+    await axios.post(
+      "http://localhost:3000/api/auth/signup",
+      {
+        name: userInfo.name,
+        email: userInfo.email,
+        password: userInfo.password,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    window.location.reload();
+    console.log(userInfo);
+  };
   return (
     <div className="flex items-center justify-center h-screen max-w-130 mx-auto ">
       <div className={cn("flex flex-col gap-6 w-full", className)} {...props}>
@@ -32,8 +54,8 @@ export function RegisterForm({
             <form
               className="p-6 md:p-8"
               onSubmit={(e) => {
-                e.preventDefault();
-                // handleRegister();
+                e.preventDefault(); // ⛔ stop browser refresh
+                handleRegister();
               }}
             >
               <FieldGroup>
@@ -103,18 +125,13 @@ export function RegisterForm({
                   />
                 </Field>
                 <Field>
-                  <Button type="submit">
-                    {registerConstant.signUp}
-                  </Button>
+                  <Button type="submit">{registerConstant.signUp}</Button>
                 </Field>
                 <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
                   {registerConstant.or}
                 </FieldSeparator>
                 <Field className="grid grid-cols-1">
-                  <Button
-                    variant="outline"
-                    type="button"
-                  >
+                  <Button variant="outline" type="button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path
                         d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
